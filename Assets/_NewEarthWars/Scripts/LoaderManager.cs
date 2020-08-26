12dfef9961d7f55418a3a07f452755d3766b8ee0
R3vivalSufra <elsufra2012@gmail.com>
2020-08-25 22:34:32 -0500

    Load Screen Setup and some Folder setup

---
+++ Assets/_NewEarthWars/Scripts/LoaderManager.cs
@@ -0,0 +1,59 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Uncomment the next line to hide the script in the inspector
+//[HideMonoScript]
+public class LoaderManager : MonoBehaviour
+{
+    [Required]
+    public Scenes currentScene;
+    [Required]
+    public TextMeshProUGUI progressText;
+
+    [HideInInspector]
+    public delegate void ProgressDelegate(float progress);
+
+
+    public void LoadScene(Scenes sceneToLoad) {
+        switch (sceneToLoad) {
+            case Scenes.Loader:
+                StartCoroutine(LoadSceneAsync(Scenes.Loader.ToString(), OnLoadLevelProgressUpdate));
+                break;
+            case Scenes.Lobby:
+                StartCoroutine(LoadSceneAsync(Scenes.Lobby.ToString(), OnLoadLevelProgressUpdate));
+                break;
+            case Scenes.CharacterSelect:
+                StartCoroutine(LoadSceneAsync(Scenes.CharacterSelect.ToString(), OnLoadLevelProgressUpdate));
+                break;
+            case Scenes.Gameplay:
+                StartCoroutine(LoadSceneAsync(Scenes.Gameplay.ToString(), OnLoadLevelProgressUpdate));
+                break;
+        }
+    }
+
+    public static IEnumerator LoadSceneAsync(string nextLevel, ProgressDelegate progressDelegate) {
+        AsyncOperation async = SceneManager.LoadSceneAsync(nextLevel);
+
+        while (!async.isDone) {
+            progressDelegate(async.progress);
+            async.allowSceneActivation = async.progress > 0.8f;
+            yield return null;
+        }
+
+    }
+
+    private void OnLoadLevelProgressUpdate(float progress) {
+        progressText.text = (int)progress + " %";
+    }
+
+    public enum Scenes {
+        Loader,
+        Lobby,
+        CharacterSelect,
+        Gameplay
+    }
+}
